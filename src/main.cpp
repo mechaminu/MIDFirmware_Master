17432801c351007491ed1b83cb0d93a9d88b7892
mechaminu <lmw5201@gmail.com>
2022-12-20 07:51:00 +0900

    dd

--- src/main.cpp
+++ src/main.cpp
@@ -1,102 +1,130 @@
 #include <mbed.h>
 #include <SimpleFOC.h>
 #include <mbed_config.h>
+#include <Wire.h>
+
+/*
+* I2C Address
+*
+* E = 0x01
+* X = 0x02
+* Y = 0x03
+* Z = MASTER : Comment out #define SLAVE_ADDR
+*/
+
+// #define SLAVE_ADDR 0x01
+
+#define GEARBOX_RATIO (47.666)
+
 using namespace mbed;
 
-
-BLDCMotor motor = BLDCMotor(7);
+BLDCMotor motor = BLDCMotor(11);
 BLDCDriver6PWM driver = BLDCDriver6PWM(8, 11, 9, 12, 10, 13);
 MagneticSensorSPI sensor = MagneticSensorSPI(AS5147_SPI, 5);
 
 MbedSPI spi(4,3,2);
-MbedI2C i2c(16,17);
+MbedI2C i2c(0,1);
 
-float sensor_offset;
-void setup() {
-    sensor.init(&spi);
-    motor.linkSensor(&sensor);
+unsigned long last;
+uint8_t buf[4];
+#ifdef SLAVE_ADDR
+void rcv(int n) {
+  Serial.println(n);
+  if (n != 6) return;
+  // if (Wire.read() != 0x02) return;
 
-    driver.voltage_power_supply = 12;
-    driver.voltage_limit = 4;
-    driver.init();
-    motor.linkDriver(&driver);
+  // float val = 0;
+  // Wire.readBytes(buf,n);
 
-    motor.voltage_sensor_align = 1;
+  // memcpy(&val, buf, 4);
+  // Serial.println(val);
 
-    motor.foc_modulation = FOCModulationType::SpaceVectorPWM;
-    motor.controller = MotionControlType::torque;
+  // if (!motor.enabled) motor.enable();
+  // motor.target = val;
+  // last = millis();
 
-    Serial.begin(115200);
-    motor.useMonitoring(Serial);
-
-    gpio_pull_up(16);
-    gpio_pull_up(17);
-    i2c.setClock(400e3);
-    i2c.begin();
-
-    motor.target = 0;
-    motor.init();
-    motor.initFOC();    // TODO : Skip Align
-
-    Serial.println(F("Motor ready"));
-
-    _delay(1000);
-
-    sensor_offset = sensor.getAngle();
+  // while(Wire.available()) {
+  for(int i = 0; i < n; i++) {
+    Serial.print(Wire.read(), HEX);
+    Serial.print(',');
+  }
+  // };
+  Serial.print('\n');
 }
 
-unsigned long lastLoop, lastCmd;
+// void req() {
+//   sensor.update();
+//   float val = sensor.getAngle();
+//   byte buf[4];
+//   memcpy(buf, &val, 4);
+//   Wire.write(buf,4);
+// }
+#endif
+
+void setup() {
+  Serial.begin(115200);
+
+  #ifdef SLAVE_ADDR
+  i2c.begin(SLAVE_ADDR);
+  i2c.onReceive(rcv);
+  // i2c.onRequest(req);
+  #else
+  i2c.begin();
+  #endif
+
+  sensor.init(&spi);
+  motor.linkSensor(&sensor);
+
+  driver.voltage_power_supply = 12;
+  driver.dead_zone = 0;
+  driver.init();
+
+  motor.linkDriver(&driver);
+
+  motor.foc_modulation = FOCModulationType::SpaceVectorPWM;
+  motor.controller = MotionControlType::torque;
+
+  motor.voltage_limit = 12;
+  motor.voltage_sensor_align = 1;
+
+  motor.init();
+  /*
+  * Zero Elec
+  * E     0.56
+  * X     0.79
+  * Y     0
+  * Z     0
+  */
+  motor.initFOC();
+
+  // Serial.println(motor.zero_electric_angle);
+
+  motor.target = 0;
+  motor.disable();
+}
+
 void loop() {
-    // Max 100Hz Command Loop
-    if (millis() - lastLoop > 10) {
-        // receive command
-        float cmd[4];
-        if (Serial.available()) lastCmd = millis();
-        while (Serial.available()) 
-        {
-            if (Serial.read() == 0x02) Serial.readBytesUntil(0x04,(uint8_t*)&cmd,sizeof(cmd));
-        }
-        while (Serial.available())
-        {
-            Serial.read();
-        }
+  motor.loopFOC();
+  motor.move();
 
-        // i2c communication with slaves
-        float ang[4];
-        for (int i = 1; i <= 3; i++)
-        {
-            i2c.beginTransmission(i);
-            i2c.write((uint8_t*)&cmd[i],4);
-            i2c.endTransmission(true);
+  if (motor.enabled && millis() - last > 100) motor.disable();
 
-            if (i2c.requestFrom(0x01,6))
-            {
-                while (i2c.available())
-                {
-                    if (i2c.read() == 0x02)
-                    {
-                        i2c.readBytesUntil(0x04,(uint8_t*)&ang[i],4);
-                    }
-                }
+  #ifndef SLAVE_ADDR
+  if (Serial.available()) {
+    String str = Serial.readStringUntil('\n');
+    while (Serial.available()) Serial.read();
 
-                while (i2c.available()) i2c.read(); // clear buffer
-            }
-        }
-        ang[0] = (sensor.getAngle() - sensor_offset) / PI * 180 / 5;
-
-        // send data
-        uint8_t buf[18];
-        buf[0] = 0x02;
-        buf[17] = 0x04;
-        memcpy(buf+1,&ang,16);
-        Serial.write(buf, 18);
-
-        lastLoop = millis();
-    }
-
-    if (millis() - lastCmd) motor.target = 0;
-
-    // Motor Loop
-    motor.loopFOC();
-    motor.move();
+    Serial.print("I heard : ");
+    Serial.println(str);
+    
+    float val = atof(str.c_str());
+    // float val = 0.12345;
+    memcpy(buf, &val, 4);
+    i2c.beginTransmission(0x01);
+    i2c.write(0x02);
+    i2c.write(buf,4);
+    i2c.write(0x04);
+    i2c.endTransmission();
+  }
+  #endif
 }
